bd1c88863dbc63670a4af56086f553b07403d708
Chickenmist <wpimlatt@gmail.com>
2024-04-12 13:08:43 -0400

    Added Knuckles, Chaos Emeralds, Motobugs, and Sonic Grid Adjusted positions of images Made Tails appear at a random x position every time the game starts

--- Monogame Lesson 1 – The Basics/Game1.cs
+++ Monogame Lesson 1 – The Basics/Game1.cs
@@ -5,6 +5,14 @@ using System;
 
 namespace Monogame_Lesson_1___The_Basics
 {
+    enum Screen
+    {
+        Grid, 
+        GreenHill,
+        AngelIsland
+    }
+
+
     public class Game1 : Game
     {
         Texture2D greenHillZoneTexture;
@@ -13,13 +21,17 @@ namespace Monogame_Lesson_1___The_Basics
         Texture2D sonicTexture;
         Texture2D tailsTexture;
         Texture2D eggmanTexture;
+        Texture2D knucklesTexture;
+        Texture2D chaosEmeraldTexture;
+        Texture2D motobugTexture;
 
+        Screen screen;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
         private Random _random = new Random();
-        private int _background;
+        private int _tailsLocation;
 
         public Game1()
         {
@@ -34,13 +46,23 @@ namespace Monogame_Lesson_1___The_Basics
 
             base.Initialize();
 
+            screen = (Screen)_random.Next(3);
+
             _graphics.PreferredBackBufferWidth = 800;
             _graphics.PreferredBackBufferHeight = 500;
             _graphics.ApplyChanges();
 
-            this.Window.Title = "My First Monogame Project";
+            Window.Title = "My First Monogame Project";
 
-            _background = _random.Next(1,3);
+
+            if (screen == Screen.GreenHill)
+            {
+                _tailsLocation = _random.Next(0, 800 - tailsTexture.Width);
+            }
+            else if (screen == Screen.AngelIsland)
+            {
+                _tailsLocation = _random.Next(200, 800 - tailsTexture.Width);
+            }
         }
 
         protected override void LoadContent()
@@ -55,6 +77,10 @@ namespace Monogame_Lesson_1___The_Basics
             sonicTexture = Content.Load<Texture2D>("sonicSprite");
             tailsTexture = Content.Load<Texture2D>("tailsSprite");
             eggmanTexture = Content.Load<Texture2D>("eggmanSprite");
+            knucklesTexture = Content.Load<Texture2D>("knucklesSprite");
+            chaosEmeraldTexture = Content.Load<Texture2D>("chaosEmeraldSprite");
+            motobugTexture = Content.Load<Texture2D>("motobugSprite");
+            
         }
 
         protected override void Update(GameTime gameTime)
@@ -75,21 +101,48 @@ namespace Monogame_Lesson_1___The_Basics
 
             _spriteBatch.Begin();
 
-            if (_background == 1) //Green Hill Zone screen
+
+
+            if (screen == Screen.GreenHill)
             {
                 _spriteBatch.Draw(greenHillZoneTexture, new Vector2(0, 0), Color.White);
                 
                 _spriteBatch.Draw(eggmanTexture, new Vector2(-50,230), Color.White);
-                _spriteBatch.Draw(sonicTexture, new Vector2(400,250), Color.White);
+                _spriteBatch.Draw(sonicTexture, new Vector2(450,250), Color.White);
+                _spriteBatch.Draw(knucklesTexture, new Vector2(700, 240), Color.White);
+                _spriteBatch.Draw(chaosEmeraldTexture, new Vector2(550, 250), Color.White);
+                _spriteBatch.Draw(tailsTexture, new Vector2(_tailsLocation, 10), Color.White);
+
+                for (int i = 120; i < 300; i += 100)
+                {
+                    _spriteBatch.Draw(motobugTexture, new Vector2(i, 290), Color.White);
+                }
             }
-            else if (_background == 2) //Angel Island Zone screen
+            else if (screen == Screen.AngelIsland)
             {
                 _spriteBatch.Draw(angelIslandZoneTexture,new Vector2(0,0), Color.White);
-            }
 
-            
-            _spriteBatch.Draw(tailsTexture, new Vector2(100,30), Color.White);
-            
+                _spriteBatch.Draw(tailsTexture, new Vector2(_tailsLocation, 10), Color.White);
+                _spriteBatch.Draw(eggmanTexture, new Vector2(-50, 170), Color.White);
+                _spriteBatch.Draw(sonicTexture, new Vector2(400, 240), Color.White);
+                _spriteBatch.Draw(knucklesTexture, new Vector2(650, 230), Color.White);
+                _spriteBatch.Draw(chaosEmeraldTexture, new Vector2(500, 240), Color.White);
+
+                for (int i = 120; i < 300; i += 100)
+                {
+                    _spriteBatch.Draw(motobugTexture, new Vector2(i, 285), Color.White);
+                }
+            }
+            else if (screen == Screen.Grid)
+            {
+                for (int i = 0; i < 800 - sonicTexture.Width; i += sonicTexture.Width)
+                {
+                    for (int j = 0; j < 500 - sonicTexture.Height; j += sonicTexture.Height)
+                    {
+                        _spriteBatch.Draw(sonicTexture, new Vector2(i, j), Color.White);
+                    }
+                }
+            }
 
             _spriteBatch.End();
 
